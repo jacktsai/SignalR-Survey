2d163d6b1d03617fb20758e7397a00772260d5a7
Jack Tsai <wa0327@gmail.com>
2013-02-08 15:03:49 +0800

    initially commit

---
+++ src/SignalR.Site/App_Code/Broadcaster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using System.Threading;
+
+/// <summary>
+/// Broadcaster 的摘要描述
+/// </summary>
+public class Broadcaster : Hub
+{
+    private readonly CancellationToken cancellationToken = new CancellationToken();
+
+    public Broadcaster()
+    {
+        Task.Factory.StartNew(() =>
+        {
+        }, cancellationToken);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        cancellationToken.CanBeCanceled
+        base.Dispose(disposing);
+    }
+}
